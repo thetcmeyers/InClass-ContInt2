a64bc9fa02d97457b903bc2c085dcfc17ba8749f
Tarren Meyers <thetarrenmeyers@gmail.com>
2021-03-13 15:27:29 -0800

    think this adds the config file

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
-def add(a,b):
-	return a + b
+"""
+    Calculator app with basic methods
+"""
 
-def sub(a,b):
-	return a - b
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
